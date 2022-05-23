15504e91691154aba91a1c8733aadda5ddb1eaca
Jon Bernal <jbernalwrc@gmail.com>
2022-05-22 21:26:07 -0700

    adds strings and numbers

--- types.go
+++ types.go
@@ -7,6 +7,7 @@ func main() {
 
 
 	// ----------------- Booleans ------------------ //
+	fmt.Println("// ----- Booleans ----- //")
 	var a bool = true
 	fmt.Printf("%v, %T\n", a, a)
 	// will show up as a zero type (false)
@@ -14,7 +15,7 @@ func main() {
 	fmt.Printf("%v, %T\n", b, b)
 	
 	// ----------------- Integers ------------------ //
-	fmt.Println("Integers")
+	fmt.Println("// ----- Integers ----- //")
 	// zero type
 	var zeroType int8
 	fmt.Printf("%v, %T\n", zeroType, zeroType)
@@ -41,26 +42,8 @@ func main() {
 
 	// ! Important, cannot divide an integer and get the remainder, you must use % (modulo) to get the remainder if dividing two integers
 
-		// ----------------- Floats ------------------ //
-		fmt.Println("Floats")
-
-		// zero type
-		var zeroTypeFloat float64 // zeroTypeFloat == 0
-		fmt.Printf("%v, %T\n", zeroTypeFloat, zeroTypeFloat)
-
-		// float
-		float1 := 3.14
-		// float64 range (+- 2.23e-308 to +- 1.8E308)
-		float1 := 3.14
-
-	
-	// ----------------- Strings ------------------ //
-	fmt.Println("Strings")
-	
-	
-
-	
 	// ----------------- bit operators ------------------ //
+	fmt.Println("// ----- Bitwise operators ----- //")
 	fmt.Println("Bitwise operators")
 	d := 10
 	e := 3
@@ -71,5 +54,75 @@ func main() {
 	f := 8
 	fmt.Println(f << 1) // Bit Shifting: shift f left 1 place == 16
 	fmt.Println(f >> 1) // Bit Shifting: shift f right 1 place == 4
+
+		// ----------------- Floats ------------------ //
+		fmt.Println("// ----- Floats ----- //")
+		
+		// zero type
+		var zeroTypeFloat float64 // zeroTypeFloat == 0
+		fmt.Printf("%v, %T\n", zeroTypeFloat, zeroTypeFloat)
+		
+		// float initialized like below will be a float64
+		float1 := 3.14
+		float1 = 13.7e72
+		float1 = 2.1e14
+		fmt.Printf("%v, %T\n", float1, float1)
+		// float64 range (-3.4e+38 to 3.4e+38.)
+		var float2 float32 = 3.14 
+		fmt.Printf("%v, %T\n", float2, float2)
+		// float64 range (-1.7e+308 to +1.7e+308)
+		var float3 float64 = 3.14 
+		fmt.Printf("%v, %T\n", float3, float3)
+		
+		// Float operations
+		floatOp1 := 10.2
+		floatOp2 := 3.7
+		fmt.Println(floatOp1 + floatOp2)
+		fmt.Println(floatOp1 - floatOp2)
+		fmt.Println(floatOp1 * floatOp2)
+		fmt.Println(floatOp1 / floatOp2)
+		// Notes:
+		// always returns a float
+		// no remainder operator available
+		// No bitwise operators for floats
+		
+		// ----------------- Complex Numbers ------------------ //
+		fmt.Println("// ----- Complex Numbers ----- //")
+		
+		var c64 complex64 = 1 + 2i
+		fmt.Printf("%v, %T\n", c64, c64)
+		
+		// Operations
+		complex1 := 1 + 2i
+		complex2 := 2 + 5.2i
+		fmt.Println(complex1 + complex2)
+		fmt.Println(complex1 - complex2)
+		fmt.Println(complex1 * complex2)
+		fmt.Println(complex1 / complex2)
+		
+		fmt.Printf("%v, %T\n", real(complex1), real(complex1)) // returns just the real number part of imaginary number
+		fmt.Printf("%v, %T\n", imag(complex2), imag(complex2)) // returns just the imaginary number part
+
+		var newComplex complex128 = complex(5, 12)
+		fmt.Printf("%v, %T\n", newComplex, newComplex) // returns just the imaginary number part
+		
+		
+		
+		// ----------------- Strings ------------------ //
+		// Notes
+		// Strings are immutable
+		
+		fmt.Println("// ----- Strings ----- //")
+		s := "this is a string"
+		fmt.Printf("%v, %T\n", s, s)
+		fmt.Printf("%v, %T\n", s[2], s[2]) // strings are an alias for a byte
+		fmt.Printf("%v, %T\n", string(s[2]), string(s[2])) // to read as a string 
+
+
+	
+	
+	
+	
+
 
 }
